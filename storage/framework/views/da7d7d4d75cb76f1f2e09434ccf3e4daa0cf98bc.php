c1763b9e925a7d73951506fdc35ca6d5dd73164b
Rehan2405 <117308871+Rehan2405@users.noreply.github.com>
2022-12-20 23:41:18 +0700

    Add files via upload

---
+++ storage/framework/views/da7d7d4d75cb76f1f2e09434ccf3e4daa0cf98bc.php
@@ -0,0 +1,15 @@
+
+<?php $__env->startSection('content','home'); ?>
+
+<?php $__env->startSection('body'); ?>
+<style>
+</style>
+<body>
+    <div class="container">
+        <p>Welcome to our Journey!</p>
+    </div>
+</body>
+
+<?php $__env->stopSection(); ?>
+
+<?php echo $__env->make('template', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\Binus\Chapter 5\Webprog\project\resources\views/home.blade.php ENDPATH**/ ?>
