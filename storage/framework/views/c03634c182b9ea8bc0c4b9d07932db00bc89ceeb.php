8318785892e9c3bd9d60eaba5b279db8dff2420f
Edbert Hansel <edbert.hansel@binus.edu>
2023-01-23 21:44:00 +0700

    change map location to dynamic

--- storage/framework/views/c03634c182b9ea8bc0c4b9d07932db00bc89ceeb.php
+++ storage/framework/views/c03634c182b9ea8bc0c4b9d07932db00bc89ceeb.php
@@ -123,7 +123,7 @@
         <span class="m-5"></span>
         <div class="mapouter w-100">
             <div class="gmap_canvas">
-            <iframe width="500" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+            <iframe width="500" height="500" id="gmap_canvas" src=<?php echo e($destination->location); ?>  frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
             </div>
         </div>
     </div>
